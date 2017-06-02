ccd56e3a585bd7a88556d4c0cf2f4a138b7d9157
jaig-sticky <jerome.aiguillon@stickyads.tv>
2017-06-02 14:03:11 +0200

    add templating

--- handlers.go
+++ handlers.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-        "fmt"
         "net/http"
+        "html/template"
     )
 
+type Page struct {
+    Name string
+}
+
+var templates = template.Must( template.ParseFiles("./templates/index.html"))
+
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Welcome !")
+    page := Page{Name:"qamt"}
+    if name := r.FormValue("name"); name != "" {
+        page.Name = name
+    }
+
+    if err := templates.ExecuteTemplate(w, "index.html", page); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
